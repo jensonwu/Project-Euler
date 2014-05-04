0cd6397db05e1e467feebbb05bc3fd47f2656666
Sid <sid@ubnt.com>
2014-05-04 15:14:41 +0800

    project euler 1

--- erlang/pe1.erl
+++ erlang/pe1.erl
@@ -1,2 +1,14 @@
--module(tut).
--export([pe1/1]).
+-module(pe1).
+-export([solve/0]).
+
+solve() ->
+    pe1(999, 0).
+
+pe1(Num, Sum) when Num > 0, Num rem 3 =:= 0; Num rem 5 =:= 0 ->
+    pe1(Num-1, Sum+Num);
+
+pe1(Num, Sum) when Num > 0 ->
+    pe1(Num-1, Sum);
+
+pe1(_, Sum) ->
+    io:format("~w~n", [Sum]).
